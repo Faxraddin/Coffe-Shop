4ca66f2ae55e105726dcc88cca655398262b4af5
Faxraddin <reydivonay@gmail.com>
2024-06-20 19:52:34 +0400

    structure

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomePage from "./Home/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="w-full h-full flex items-center justify-center">
+      <HomePage />
     </main>
   );
 }
