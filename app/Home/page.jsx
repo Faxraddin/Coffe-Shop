4e8ac4ca741e9709b1ee9353c52a1b276fdfee0a
Faxraddin <reydivonay@gmail.com>
2024-06-21 21:48:04 +0400

    2 section

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import Image from "next/image";
 import Navbar from "../Layout/Navbar";
 
+const Product = ({src,name,price}) => {
+  return(
+    <div className="border flex sm:p-2 mt-7 relative flex-col justify-end sm:h-[250px] h-[300px] items-center text-center p-5 w-[250px] sm:w-[160px] rounded-[30px]">
+      <Image className="sm:w-[100px] h-auto absolute w-[140px] -top-7" src={`/${src}`} width={2000} height={2000} alt="product"/>
+      <div>
+        <span className="font-light text-[14px]">Blend</span>
+        <h1 className="font-bold text-[20px]">{name}</h1>
+        <p className="font-light text-[21px]">{price}</p>
+      </div>
+    </div>
+  )
+}
+
 const HomePage = () => {
   return (
     <div className="w-full h-screen overflow-y-scroll snap-y snap-mandatory">
       <section className="flex snap-start 2lg:h-auto sm:h-screen justify-center 2lg:gap-3 gap-20 flex-col items-center w-full h-screen">
         <Navbar />
         <div className="2lg:flex-col 2lg:text-center w-[90%] relative h-full flex justify-between items-center">
-          <div className="h-full 2lg:w-[100%] sm:justify-center sm:gap-7 2lg:items-center flex flex-col gap-5 w-[50%]">
+          <div className="h-full 2lg:w-[100%] sm:justify-center sm:gap-7 2lg:items-center big:justify-center flex big:-mt-40 flex-col gap-5 w-[55%]">
             <Image width={2000} height={2000} className="w-[37vw] sm:w-[250px] 2lg:w-[60vw] h-auto" alt="HeroImg" src={'/HeroText.png'} />
             <h1 className="text-[60px] md:text-[8vw] font-extrabold text-[#371406]">An Online Coffee Shop</h1>
             <p>Straight to your doorstep. We don't roast our beans until we have your order. Every ore trusted the slipped the some auy.</p>
@@ -19,13 +32,29 @@ const HomePage = () => {
               <span className="flex px-7 flex-col sm:px-3 text-[18px] justify-center sm:text-[15px] items-center font-light">Total sales <h3 className="font-bold text-[#371406] sm:text-[20px] text-[33px]">340k+</h3></span>
             </div>
           </div>
-          <Image alt="cup" src={'/Cup.png'} width={2000} height={2000} className="2lg:relative 2lg:hidden 2lg:top-0 2lg:right-0 w-[45vw] absolute -right-24 -top-[5vw] h-auto" />
+          <Image alt="cup" src={'/Cup.png'} width={2000} height={2000} className="2lg:relative 2lg:hidden 2lg:top-0 2lg:right-0 w-[45vw] absolute -right-24 big:top-24 -top-[5vw] h-auto" />
           <Image alt="cup2" src={'/Cup2.png'} width={2000} height={2000} className="2lg:relative sm:w-[250px] hidden 2lg:flex 2lg:top-0 2lg:right-0 w-[45vw] absolute -right-24 -top-[5vw] h-auto" />
         </div>
       </section>
 
-      <section className="w-full snap-start h-screen">
+      <section className="w-full flex flex-col snap-start h-screen 2lg:h-auto sm:h-scree">
         <Image width={2000} height={2000} className="w-[100%] h-auto" alt="Line" src={'/Line.png'} />
+        <div className="w-full h-full flex gap-4 justify-center flex-col items-center">
+          <Image width={2000} height={2000} className="w-[50px] h-auto" alt="Point" src={'/Point.png'} />
+          <div className="flex w-[95%] gap-3 text-[#371406] flex-col text-center">
+            <h1 className="text-[40px] md:text-[8vw] font-bold">Explore the recent products</h1>
+            <h2 className="text-[17px] sm:px-3 font-light">Our delectable drink options, includino classic espresso choices, house soeciolties, truit smoothies and frozen treats.</h2>
+            <div className="mt-10 flex flex-wrap justify-center gap-5 items-center">
+              <Product src={'P1.png'} name={'Spice iceland blend'} price={'$12'}/>
+              <Product src={'P2.png'} name={'Hair blender'} price={'$12'}/>
+              <Product src={'P3.png'} name={'Col brew blend'} price={'$16'}/>
+              <Product src={'P4.png'} name={'Honduras EL Puente'} price={'$80'}/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full border flex flex-col snap-start h-screen">
+
       </section>
     </div>
   );
