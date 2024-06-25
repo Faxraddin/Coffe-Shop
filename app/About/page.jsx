c8ee5baa6978e3afa32d9407a359138df9783d8e
Faxraddin <reydivonay@gmail.com>
2024-06-25 18:37:46 +0400

    s

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -41,11 +41,11 @@ const About = () => {
         <div className="w-full overflow-x-hidden h-screen overflow-y-scroll snap-y snap-mandatory">
             <section
                 style={{ backgroundImage: 'url("/BG.jpg")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
-                className="flex snap-start 2lg:h-auto sm:h-auto justify-center 2lg:gap-3 gap-20 flex-col items-center w-full h-screen">
+                className="flex overflow-hidden snap-start lg:h-auto sm:h-auto justify-center 2lg:gap-3 gap-20 flex-col items-center w-full h-screen">
                 <Navbar />
                 <div className="w-full h-full flex justify-center items-center">
-                    <Image style={hand1Transform} className="w-auto h-full" src={'/Hand1.png'} alt="hand1" width={2000} height={2000}/>
-                    <Image style={hand2Transform} className="w-auto h-full" src={'/Hand2.png'} alt="hand2" width={2000} height={2000}/>
+                    <Image style={hand1Transform} className="w-auto xs:left-9 relative top-2 h-full" src={'/Hand1.png'} alt="hand1" width={2000} height={2000}/>
+                    <Image style={hand2Transform} className="w-auto xs:right-9 relative top-4 h-full" src={'/Hand2.png'} alt="hand2" width={2000} height={2000}/>
                 </div>
             </section>
             <section className="flex snap-start 2lg:h-auto sm:h-auto justify-center 2lg:gap-3 gap-20 flex-col items-center w-full h-screen">
