5a3cab34fa666b95e678f71388b7ce579d32800b
Faxraddin <reydivonay@gmail.com>
2024-06-27 21:35:20 +0400

    w footer

--- app/Layout/Footer.jsx
+++ app/Layout/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <div className="flex lg:flex-col lg:gap-10 justify-between">
           <div className="mb-0">
             <a href="/" className="flex items-center gap-1">
-              <h1 className={`sm:gap-2 flex cursor-pointer justify-center items-center gap-3 text-white text-[23px] font-extralight`}><Image onClick={() => { router.push('/') }} src={'/Logo.png'} width={2000} className="w-[80px] rounded-full h-auto relative bottom-1" priority={true} height={2000} alt="logo" />COFFEO</h1>
+              <h1 className={`sm:gap-2 flex cursor-pointer justify-center items-center gap-3 text-white text-[23px] font-extralight`}><Image onClick={() => { router.push('/') }} src={'/Logo.png'} width={2000} className="w-[80px] sm:w-[50px] rounded-full h-auto relative bottom-1" priority={true} height={2000} alt="logo" />COFFEO</h1>
             </a>
           </div>
           <div className="grid gap-8 xs:grid-cols-1 lg:grid-cols-2 lg:gap-20 sm:gap-6 grid-cols-2">
@@ -21,11 +21,11 @@ const Footer = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow Us</h2>
               <ul className="text-white font-medium">
                 <li className="mb-4">
-                  <a href="https://www.instagram.com/brizgroupllc/"
+                  <a href=""
                     className="hover:underline ">Instagram</a>
                 </li>
                 <li>
-                  <a href="https://www.facebook.com/brizgroupllc"
+                  <a href=""
                     className="hover:underline">Facebook</a>
                 </li>
               </ul>
@@ -46,20 +46,6 @@ const Footer = () => {
         <hr className="my-6 border-white sm:mx-auto lg:my-8" />
         <div className="items-center sm:flex-col sm:gap-5 sm:justify-start sm:items-start flex justify-between">
           <span className="text-sm flex flex-wrap gap-2 text-white">© 2024 <a href="/" className="hover:underline">COFFEO. All Rights Reserved.</a></span>
-          <div className="flex gap-3 text-white justify-center mt-0">
-            <a href="https://www.facebook.com/brizgroupllc" className="text-[23px]">
-              <FaFacebook className="sm:text-[24px] text-[25px]" />
-            </a>
-            <a href="https://www.instagram.com/brizgroupllc/" className="text-[23px]">
-              <AiFillInstagram className="sm:text-[27px] text-[25px]" />
-            </a>
-            <a href="https://wa.me/+994509771313" className="text-[23px]">
-              <IoLogoWhatsapp className="sm:text-[24px] text-[25px]" />
-            </a>
-            <a href="https://www.linkedin.com/company/b-r-i-z-group-llc/" className="text-[23px]">
-              <FaLinkedin className="sm:text-[24px] text-[25px]" />
-            </a>
-          </div>
         </div>
       </div>
     </footer>
